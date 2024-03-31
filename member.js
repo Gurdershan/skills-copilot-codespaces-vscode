35e065b5e0002a306bf2ebf33d3acca966a7bef1
Gurdershan <gurdershan@gmail.com>
2024-03-31 04:36:49 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember() {
+  var member = {
+    name: "John Doe",
+    age: 25,
+    skills: ["HTML", "CSS", "JS"],
+    greet: function() {
+      console.log(`Hello, my name is ${this.name}`);
+    },
+  };
+
+  return member;
+}
+
+function showSkills() {
+  var member = skillsMember();
+  console.log(member.skills);
+}
