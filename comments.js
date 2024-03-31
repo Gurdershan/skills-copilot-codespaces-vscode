cb7fc7496eeb1a1d2b1bd147e22ab7a4b0f4c78b
Gurdershan <gurdershan@gmail.com>
2024-03-31 05:02:21 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,25 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.html file.
+// The comments.html file should be in the same directory as the comments.js file.
+// The comments.html file should have the following content:
+
+// <!DOCTYPE html>
+// <html>
+// <head>
+//   <title>Comments</title>
+// </head>
+// <body>
+//   <h1>Comments</h1>
+//   <ul id="comments">
+//   </ul>
+//   <form id="comment-form">
+//     <input type="text" id="new-comment" placeholder="Your comment here">
+//     <input type="submit" value="Add Comment">
+//   </form>
+//   <script src="comments.js"></script>
+// </body>
+// </html>
+// The comments.js file should have the following content:
+
+// const http = require('http');
+// const fs = require('fs');
